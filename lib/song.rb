549d1a1afd7ba68326f86d9c47a52b1cedbf0be8
Caroline Longnecker <cvrieous@gmail.com>
2019-12-27 22:05:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,13 +11,13 @@ class Song
   end
 
   def self.create
-    song = Song.new.save
+    song = Song.new
+    song.save
     song
   end
 
   def self.new_by_name(name)
     song = Song.new
-  
     song.save
     song
   end
